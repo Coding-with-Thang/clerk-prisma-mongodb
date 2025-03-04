8b27548a520e7ceea813e564bcc6b3ab1817f6fe
Coding With Thang <codingwiththang@gmail.com>
2025-03-03 21:27:13 -0600

    Added function to create mongodb entry when new user created

--- src/app/api/webhooks/clerk/route.js
+++ src/app/api/webhooks/clerk/route.js
@@ -1,8 +1,87 @@
 import { clerkClient } from "@clerk/nextjs";
-import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 import { createUser } from "../../../../lib/actions/user.action";
 
-export async function POST(req) {}
+export async function POST(req) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env"
+    );
+  }
+
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // Get headers
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error: Missing Svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  let evt;
+
+  // Verify payload with headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    });
+  } catch (err) {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
+      status: 400,
+    });
+  }
+
+  // Do something with payload
+  // For this guide, log payload to console
+  const { id } = evt.data;
+  const eventType = evt.type;
+
+  //Create User in MongoDB
+  if (eventType === "user.created") {
+    const { id, username, first_name, last_name, image_url } = evt.data;
+
+    const user = {
+      clerkId: id,
+      username,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
+
+    console.log("New user: ", user);
+
+    const newUser = await createUser(user);
+
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
+    return NextResponse.json({ message: "New user created", newUser });
+  }
+
+  console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
+  console.log("Webhook payload:", body);
+
+  return new Response("Webhook received", { status: 200 });
+}
