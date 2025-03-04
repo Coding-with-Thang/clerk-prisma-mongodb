8657d4323d35071c739706aa79635b840c81aa84
Coding With Thang <codingwiththang@gmail.com>
2025-03-03 21:37:56 -0600

    Changed mongodb connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,28 +1,21 @@
 import mongoose from "mongoose";
 
+let initialized = false;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
 export const connect = async () => {
-  if (cached.conn) return cached.conn;
-
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
+  mongoose.set("strictQuery", true);
+  if (initialized) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+  try {
+    await mongoose.connect(MONGODB_URI, {
       dbName: "clerk-prisma-db",
-      bufferCommands: false,
-      connectTimeoutMS: 30000,
     });
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+    console.log("Connected to MongoDB");
+    initialized = true;
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
 };
